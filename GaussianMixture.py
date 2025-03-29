7cc2019b4f97f2381ff5326fa03de394143e0a73
tiffie1 <gdmaster17@gmail.com>
2025-03-29 01:06:53 -0400

    Implement gaussian mixture model in GaussianMixture.py

--- GaussianMixture.py
+++ GaussianMixture.py
@@ -1,20 +1,20 @@
 from typing import cast
 
+import numpy as np
+import pandas as pd
 from pandas import DataFrame, read_csv
+from scipy.sparse import csr_matrix
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.mixture import GaussianMixture
 from sklearn.model_selection import train_test_split
 
-from Preprocessing import clean_text
+from GraphicsUtils import bar_binary_plot
 
 if __name__ == "__main__":
     # columns = 'id', 'label', 'tweet'
-    data = read_csv("./data/twitter.csv")
+    data = read_csv("./data/twitter_clean.csv")
     data = data.set_index("id")
-
-    data["tweet"] = data["tweet"].apply(clean_text)
-
-    # X_train.to_csv("./data/twitter_clean.csv")
+    data = data.dropna(subset=["tweet"])
 
     # 60%, 20%, 20% split
     data_train, data_temp = cast(
@@ -28,9 +28,25 @@ if __name__ == "__main__":
 
     vectorizer = CountVectorizer(binary=True, min_df=5)
 
-    X_train = vectorizer.fit_transform(data_train["tweet"])
-    X_dev = vectorizer.fit_transform(data_dev["tweet"])
-    X_test = vectorizer.fit_transform(data_test["tweet"])
-    y_train = data_train["id"]
-    y_dev = data_dev["id"]
-    y_test_true = data_test["id"]
+    # Create bag of words feature matrices.
+    X_train = vectorizer.fit_transform(data_train["tweet"]).toarray()  # type: ignore[attr-defined]
+    X_train = np.array(X_train)
+
+    X_dev: csr_matrix = vectorizer.transform(data_dev["tweet"])  # type: ignore[attr-defined]
+    X_dev = X_dev.toarray()
+    X_dev = np.array(X_dev)
+
+    X_test = vectorizer.transform(data_test["tweet"])  # type: ignore[attr-defined]
+    X_test = X_test.toarray()
+    X_test = np.array(X_test)
+
+    y_train = data_train["label"]
+    y_dev = data_dev["label"]
+    y_test_true = data_test["label"]
+
+
+    model = GaussianMixture(2)
+
+    model.fit(X_train, y_train)
+    y_test_pred = model.predict(X_test)
+    bar_binary_plot(pd.Series(y_test_pred), "pred id distribution", "id")
